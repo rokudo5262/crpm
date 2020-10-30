8cc07211130e71bcae191bcfb2e6cab0b6d32c85
Thien Nguyen <thiennguyen0897@gmail.com>
2020-10-30 15:31:32 +0700

    logger

--- application/controllers/admin/Logger.php
+++ application/controllers/admin/Logger.php
@@ -4,30 +4,80 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Logger extends AdminController
 {
+
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('expenses_model');
+        $this->load->model('tasks_model');
     }
 
-    public function index($id = '')
+    public function index($param = '')
     {
-        echo 'asd';
-    }
+        $chosen_ones = [
+            '41', // troy
+            '43', // khane
+            '47', // thomas
+            '51', // wall
+            '68', // leo
+            '79', // pobby
+            '70', // nami
+            '64', // tacy
+            '33', // mark
 
-    public function list_expenses($id = '')
-    {
+        ];
+        if(!in_array(get_staff_user_id(), $chosen_ones))
+            redirect(site_url('/admin'));
+
         close_setup_menu();
 
-        if (!has_permission('expenses', '', 'view') && !has_permission('expenses', '', 'view_own')) {
-            access_denied('expenses');
+        $tasks = $this->tasks_model->get_user_tasks_assigned();
+        $data['tasks'] = $tasks;
+        $this->load->view('admin/logger/logger', $data);
+    }
+
+    public function isWeekend($timestamp) {
+        $day = date("D", $timestamp);
+        if($day == 'Sat' || $day == 'Sun'){
+            return true;
         }
+        return false;
+    }
 
-        $data['expenseid']  = $id;
-        $data['categories'] = $this->expenses_model->get_category();
-        $data['years']      = $this->expenses_model->get_expenses_years();
-        $data['title']      = _l('expenses');
-
-        $this->load->view('admin/expenses/manage', $data);
+    public function postQuickLog()
+    {
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $data["task_id"] = $this->input->post('task_id');
+        $data["log_month"] = $this->input->post('log_month');
+        $data["log_start"] = $this->input->post('log_start');
+        $data["log_end"] = $this->input->post('log_end');
+        $start_day = intval($data["log_start"]);
+        $end_day = intval($data["log_end"]);
+        for ($i = $start_day; $i <= $end_day; $i++) {
+            $start_time_1 = $data["log_month"] . "/" . $i . "/" . date('Y') . " 08:30";
+            $end_time_1 = $data["log_month"] . "/" . $i . "/" . date('Y') . " 12:00";
+            $start_time_2 = $data["log_month"] . "/" . $i . "/" . date('Y') . " 13:00";
+            $end_time_2 = $data["log_month"] . "/" . $i . "/" . date('Y') . " 17:30";
+            if(!$this->isWeekend(strtotime($start_time_1))) {
+                $this->db->insert(db_prefix() . 'taskstimers', [
+                    'start_time' => strtotime($start_time_1),
+                    'end_time' => strtotime($end_time_1),
+                    'staff_id' => get_staff_user_id(),
+                    'task_id' => $data['task_id'],
+                    'hourly_rate' => 0,
+                    'note' => (isset($data['note']) && $data['note'] != '' ? nl2br($data['note']) : null),
+                ]);
+                $this->db->insert(db_prefix() . 'taskstimers', [
+                    'start_time' => strtotime($start_time_2),
+                    'end_time' => strtotime($end_time_2),
+                    'staff_id' => get_staff_user_id(),
+                    'task_id' => $data['task_id'],
+                    'hourly_rate' => 0,
+                    'note' => (isset($data['note']) && $data['note'] != '' ? nl2br($data['note']) : null),
+                ]);
+            } else {
+                continue;
+            }
+        }
+        echo "Log successful!";
     }
 }
--- application/views/admin/logger/logger.php
+++ application/views/admin/logger/logger.php
@@ -1,13 +1,93 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<?php init_head(); ?>
+<div id="wrapper">
+    <div class="content">
+        <div class="row">
+            <h2>Công cụ log time tự động</h2>
+            <p>Xin chào, đây là một công cụ không được phổ biến rộng rãi.</p>
+            <div class="row">
+                <div class="col-md-5">
+                    <form method="POST">
+                        <h3>Log nhanh</h3>
+                        <p>Dành cho người lười hoặc tháng vừa rồi chỉ làm một task.</p>
+                        <p>Chọn một task và chọn số ngày để log (tự động log mỗi ngày 8 tiếng, bỏ T7 CN).</p>
+                        <p>
+                            <label for="task_list">Danh sách task:</label>
+                            <select id="task_list" name="task_list" class="form-control">
+                                <?php
+                                foreach ($tasks as $task) {
+                                    ?>
+                                    <option value="<?= $task['id'] ?>"><?= $task['name'] ?></option>
+                                    <?php
+                                }
+                                ?>
+                            </select>
+                        </p>
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <p>
+                                    <label for="log_month">Tháng:</label>
+                                    <input type="number" min="1" max="12" class="form-control" name="log_month"
+                                           id="log_month" value="<?= date('m') ?>" required>
+                                </p></div>
+                            <div class="col-sm-4">
+                                <p>
+                                    <label for="log_start">Ngày bắt đầu:</label>
+                                    <input type="text" class="form-control" name="log_start"
+                                           id="log_start" value="1" required>
+                                </p>
+                            </div>
+                            <div class="col-sm-4">
+                                <p>
+                                    <label for="log_end">Ngày kết thúc:</label>
+                                    <input type="text" class="form-control" name="log_end"
+                                           id="log_end" value="<?= date('t') ?>" required>
+                                </p>
+                            </div>
+                        </div>
+                        <p>
+                            <button type="submit" class="btn btn-primary" name="submit_quicklog" id="submit_quicklog">
+                                Start Logging!
+                            </button>
+                        </p>
+                    </form>
+                </div>
+                <div class="col-md-1"></div>
+                <div class="col-md-5" style="border-left: 2px black dotted;">
+                    <h3>Log thủ công</h3>
+                    <p>Dành cho người cần cù bù siêng năng.</p>
+                </div>
+                <div class="col-md-1"></div>
+            </div>
 
+        </div>
+    </div>
+</div>
+<?php init_tail(); ?>
+<script type="text/javascript">
+    $('form').submit(function (e) {
+        e.preventDefault();
+        var task_id = $('#task_list').val();
+        var log_month = $('#log_month').val();
+        var log_start = $('#log_start').val();
+        var log_end = $('#log_end').val();
+        $.ajax({
+            url: '/admin/logger/postQuickLog',
+            type: 'POST',
+            data: {
+                task_id: task_id,
+                log_month: log_month,
+                log_start: log_start,
+                log_end: log_end
+            },
+            error: function () {
+                alert('Something is wrong');
+            },
+            success: function (data) {
+                alert(data);
+            }
+        });
+    });
+</script>
 </body>
 </html>
