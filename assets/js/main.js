d5d297ceb3a938b1a1c801986d4ffff3ee02f8e1
Troy Nguyen <troy@reputable.asia>
2021-01-18 12:03:01 +0700

    Fix json response comment Noti Slack

--- assets/js/main.js
+++ assets/js/main.js
@@ -5030,6 +5030,7 @@ function add_task_comment(task_id) {
     data.taskid = task_id;
     $.post(admin_url + 'tasks/add_task_comment', data).done(function (response) {
         try {
+            response = response.replace('ok{','{');
             response = JSON.parse(response);
         } catch (e) {
             response = response.replace('ok{','{');
