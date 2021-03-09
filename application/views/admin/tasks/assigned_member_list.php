d3b88a43724ef92f65035879b1cb869780783c08
Troy Nguyen <troy@reputable.asia>
2021-03-09 12:15:39 +0700

    Add "All" filter to assignee list, save and reload assignees filter after referesh page or revisit

--- application/views/admin/tasks/assigned_member_list.php
+++ application/views/admin/tasks/assigned_member_list.php
@@ -1,3 +1,7 @@
+<li class="task_assigned_all">
+    <a href="#" data-cview="task_assigned_all"
+       onclick="kb_custom_view('task_assigned_all','task_assigned_all'); return false;">All</a>
+</li>
 <?php foreach ($tasks_filter_assignees as $as) { ?>
 <li class="task_assigned_<?php echo $as['assigneeid']; ?>">
     <a href="#" data-cview="task_assigned_<?php echo $as['assigneeid']; ?>"
