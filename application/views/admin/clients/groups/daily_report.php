86cb49731abebeaa12bd059fd7ea383c5639eefc
Nicholas <nicholas@reputable.asia>
2021-03-18 13:00:49 +0700

    update hide logo google

--- application/views/admin/clients/groups/daily_report.php
+++ application/views/admin/clients/groups/daily_report.php
@@ -8,7 +8,8 @@ if(isset($client)){ ?>
             height:900px !important;
         }
         .footer {
-            width:650px !important;
+            width: 95% !important;
+            height: 5% !important;
         }      
     }
     @media screen and (min-device-width: 1400px)  and (max-device-width: 1599px) { 
@@ -17,47 +18,48 @@ if(isset($client)){ ?>
             height:1200px !important;
         }
         .footer {
-            width:850px !important;
+            width: 95% !important;
+            height: 5% !important;
         }        
     }
     @media only screen and (min-device-width: 320px) and (max-device-width: 374px) { 
         .responsive iframe {
             width: 100% !important;
-            height:400px !important;
+            height:450px !important;
         }
         .footer {
-            width:100% !important;
-            height:25px !important;
+            width: 90% !important;
+            height: 6.6% !important;
         }
     }
     @media only screen and (min-device-width: 411px) and (max-device-width: 665px) { 
         .responsive iframe {
             width: 100% !important;
-            height:800px !important;
+            height:810px !important;
         }
         .footer {
-            height:23px !important;
-            width:100% !important;
+            height: 4.3% !important;
+            width: 95% !important;
         }
     }
     @media only screen and (min-device-width: 666px) and (max-device-width: 767px) { 
         .responsive iframe {
             width: 100% !important;
-            height:900px !important;
+            height:950px !important;
         }
         .footer {
-            height:23px !important;
-            width:100% !important;
+            height: 4% !important;
+            width: 95% !important;
         }
     }
     @media only screen and (min-device-width: 375px) and (max-device-width: 410px) { 
         .responsive iframe {
             width: 100% !important;
-            height:450px !important;
+            height:550px !important;
         }
         .footer {
-            height:25px !important;
-            width:100% !important;
+            height:6% !important;
+            width:90% !important;
         }
     }
     @media only screen and (min-device-width: 768px) and (max-device-width: 1023px){
@@ -66,7 +68,8 @@ if(isset($client)){ ?>
             height:1200px !important;
         }
         .footer {
-            width:100% !important;
+            height:6% !important;
+            width:97% !important;
         }
     }
     @media screen and (min-width: 4001px) {
@@ -74,34 +77,40 @@ if(isset($client)){ ?>
             width: 1700px !important; height:2400px !important;
         }
         .footer {
-            width:1700px !important;
+            height:6% !important;
+            width:97% !important;
         }
     }
-    @media screen and (min-width: 2501px) and (max-width: 4000px) {
+    @media screen and (min-width: 2500px) and (max-width: 4000px) {
         .responsive iframe {
             width: 1500px !important; height:2300px !important;
         }
         .footer {
-            width:1500px !important;
+            height:6% !important;
+            width:97% !important;
         }
     }
-    @media screen and (min-width: 1600px) and (max-width: 2500px) {
+    @media screen and (min-width: 1600px) and (max-width: 2499px) {
         .responsive iframe {
             width: 1000px !important; height:1500px !important;
         }
         .footer {
-            width:1000px !important;
+            height:5% !important;
+            width:90% !important;
         }
     }
 </style>
+<h4 class="customer-profile-group-heading">Daily Report</h4>
 <div class="col-md-4">
-        <label for="website">Daily Report Embed</label>
+    <div class="form-group">
+        <label for="website">Embed Code</label>
         <div class="input-group">
         <input type="text" name="ifame_report" id="ifame_report" value="<?php echo $client->daily_report_embed; ?>" class="form-control">
         <button class="btn btn-info label-margin" onclick="save_ifame_report(<?php echo $client->userid; ?>); return false;">
     <?php echo _l('submit'); ?></button>
+        </div>
+    </div>
 </div>
-<br/>
 <div class="responsive">
 <iframe src="<?php echo $client->daily_report_embed; ?>" frameborder="0" style="border:0" allowfullscreen>
 </iframe>
--- application/views/themes/perfex/views/dailyreport.php
+++ application/views/themes/perfex/views/dailyreport.php
@@ -3,65 +3,72 @@
     @media screen and (min-device-width: 1024px)  and (max-device-width: 1399px) { 
         .responsive iframe {
             width: 100% !important;
-            height:1350px !important;
+            height:1700px !important;
         }
         .footer_hide {
-            bottom: 108px !important;
-            width: 94% !important;
-            height:2% !important;
+            width: 97% !important;
+            height: 5% !important;
             }
     }
     @media screen and (min-device-width: 1400px)  and (max-device-width: 1599px) { 
         .responsive iframe {
-            width: 100% !important;
+            width: 99.9% !important;
             height:1600px !important;
         }
         .footer_hide {
-            bottom: 108px !important;
-            width: 1170px !important;
-            height:2% !important;
+            width: 98% !important;
+            height: 5% !important;
             }
     }
     @media only screen and (min-device-width: 320px) and (max-device-width: 374px) { 
         .responsive iframe {
-            width: 100% !important;
-            height:470px !important;
+            width: 98% !important;
+            height:500px !important;
         }
         .footer_hide {
-            bottom: 44px !important;
-            width: 91% !important;
-            height: 25px !important;
+            width: 90% !important;
+            height: 8% !important;
             }
     }
+    
     @media only screen and (min-device-width: 411px) and (max-device-width: 665px) { 
         .responsive iframe {
-            width: 100% !important;
-            height:700px !important;
+            width: 98% !important;
+            height: 800px !important;
         }
         .footer_hide {
-            bottom: 44px !important;
             width: 93% !important;
-            height:3% !important;
+            height: 5% !important;
+            }
+    }
+    @media only screen and (min-device-width: 666px) and (max-device-width: 767px) { 
+        .responsive iframe {
+            width: 98% !important;
+            height: 950px !important;
+        }
+        .footer_hide {
+            width: 94% !important;
+            height: 5% !important;
             }
     }
     @media only screen and (min-device-width: 375px) and (max-device-width: 410px) { 
         .responsive iframe {
-            width: 100% !important;
-            height:550px !important;
+            width: 98% !important;
+            height: 800px !important;
         }
         .footer_hide {
-            bottom: 44px !important;
-            width: 92% !important; 
+            width: 93% !important;
+            height: 5% !important;
             }
     }
     @media only screen and (min-device-width: 768px) and (max-device-width: 1023px){
         .responsive iframe {
             width: 100% !important;
-            height:1200px !important;
+            height:1300px !important;
         }
         .footer_hide {
-            bottom: 44px !important;
-            width: 97.5% !important; 
+            height: 5% !important;
+            width: 96% !important; 
             }
     }
     @media screen and (min-width: 4001px) {
@@ -70,8 +77,8 @@
             height:1700px !important;
         }
         .footer_hide {
-            bottom: 108px !important;
-            width: 1170px !important; 
+            height: 5% !important;
+            width: 97% !important; 
             }
     }
     @media screen and (min-width: 2500px) and (max-width: 4000px) {
@@ -79,8 +86,8 @@
             width: 100% !important; height:1700px !important;
         }
         .footer_hide {
-            bottom: 108px !important;
-            width: 1170px !important; 
+            height: 5% !important;
+            width: 97% !important;  
             }
     }
     @media screen and (min-width: 1600px) and (max-width: 2499px) {
@@ -88,19 +95,24 @@
             width: 100% !important; height:1700px !important;
         }
         .footer_hide {
-            bottom: 108px !important;
-            width: 1170px !important; 
+            height: 5% !important;
+            width: 97% !important; 
             }
     }
+    
 </style>
 <div class="panel_s">
-    <!-- <div class="panel-body"> -->
-    <h4 class="no-margin section-text"> Daily Report</h4><br/>
-    <!-- </div> -->
+<div class="panel_s section-heading section-estimates">
+    <div class="panel-body">
+        <h4 class="no-margin section-text">Daily Report</h4>
+    </div>
+    <div class="panel-body">
     <div class="responsive">
         <iframe width= "100%" src="<?php echo $client->daily_report_embed; ?>" frameborder="0" style="border:0" allowfullscreen> </iframe>
     </div>
-    <div class="footer_hide" style="position:absolute; bottom:4px; height:25px; width:500px; background-color:#fff; z-index:10px"> </div>
+    <div class="footer_hide" style="position:absolute; bottom:10px ;height:25px; width:500px; background-color:#fff; z-index:10px"> </div>
+    </div>
+</div>
 </div>
 
 
