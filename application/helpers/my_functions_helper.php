d5796a4532ab8ec4fa7b66267f6baa2902de2225
Trump Trinh <trump@reputable.asia>
2021-03-30 14:59:23 +0700

    fix sales estimate and sales proposals pipeline

--- application/helpers/my_functions_helper.php
+++ application/helpers/my_functions_helper.php
@@ -5,9 +5,10 @@ hooks()->add_action('app_admin_assets', 'init_assets_kanban_task_advance_filter'
 
 function init_assets_kanban_task_advance_filter() {
 	$CI = &get_instance();
-
-	// Javascript
-	$CI->app_scripts->add('kanban-task-advance-js', 'assets/js/kanban-task-advance.js');
+    if(current_url()==admin_url('tasks')) {
+        // Javascript
+        $CI->app_scripts->add('kanban-task-advance-js', 'assets/js/kanban-task-advance.js');
+    }
 }
 
 function generate_task_status_name($task_status_id) {
